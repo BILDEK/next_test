ed43795c46952f6dc6eb19d0d98b2e4b951f43b7
tayipbilaltedik <bilaltedik091@gmail.com>
2025-08-01 00:28:02 +0300

    layout.tsx new update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
-// app/layout.ts
-import '@/app/ui/global.css'; // Global CSS dosyasını içe aktarma
-
-// app/ui/fonts dosyasından lusitana fontunu içe aktarma
+// app/layout.tsx
+import '@/app/ui/global.css';
 import { lusitana } from '@/app/ui/fonts';
 
-// Next.js'in beklediği tek dışa aktarma (export default) budur.
 export default function RootLayout({
   children,
 }: {
@@ -12,9 +9,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/* Lusitana fontunu `className` olarak `body` etiketine uyguluyoruz.
-        'antialiased' sınıfı, fontların daha pürüzsüz görünmesini sağlar.
-      */}
       <body className={`${lusitana.className} antialiased`}>{children}</body>
     </html>
   );

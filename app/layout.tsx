cbbfe9f3e0153c14a2163fbbf217c6680a47d3c9
tayipbilaltedik <bilaltedik091@gmail.com>
2025-08-03 00:10:37 +0300

    we are getting progress

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,18 @@
 // app/layout.tsx
 import '@/app/ui/global.css';
-
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+ 
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+ 
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={lusitana.className}>
       <body>{children}</body>
     </html>
-  );
-}
+  )
+} 

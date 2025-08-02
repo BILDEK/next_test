7c1b382f1b02208b84cefe8d1c450d788f779bec
tayipbilaltedik <bilaltedik091@gmail.com>
2025-08-02 18:28:21 +0300

    layout stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 // app/layout.tsx
 import '@/app/ui/global.css';
-import { lusitana } from '@/app/ui/fonts';
-
+ 
 export default function RootLayout({
   children,
 }: {
@@ -9,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${lusitana.className} antialiased`}>{children}</body>
+      <body  >{children}</body>
     </html>
   );
 }
